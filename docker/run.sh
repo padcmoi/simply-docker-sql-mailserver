73a2a3f4016efd111037a0a54eb2392b4a800fda
Votre nouveau nom d'utilisateur <julien@vtest.com>
2024-12-17 17:28:14 +0100

    feat: separate dockerfiles in dedicated folder && import repository in docker container

--- docker/run.sh
+++ docker/run.sh
@@ -9,17 +9,16 @@ netstat -tulpn | grep -E -w '25|80|110|143|443|465|587|993|995|4190'
 
 # case app
 rm /var/www/html/*
-cd /webadmin/www
+cd /opt/source/www
 pnpm install
 pnpm run build
 mv dist/* /var/www/html/
 /etc/init.d/./apache2 start
 
 # case api
-cd /webadmin/api
+mv /opt/source/api /opt/api
+cd /opt/api
+cp .env.sample .env 
 pnpm install
 pnpm run build
-pnpm run start:prod
-
-
-
+node dist/main
