5f177f8af10bce0f6d1d1163438b208ea3d77ceb
padcmoi <juliennaskot@gmail.com>
2025-01-06 21:05:54 +0100

    fix: add roundcube table to mailuser mysql user

--- docker/exports/config.sql
+++ docker/exports/config.sql
@@ -8,6 +8,8 @@ GRANT ALL PRIVILEGES ON mailserver.* TO mailuser@localhost IDENTIFIED BY '____ma
 
 GRANT ALL PRIVILEGES ON opendmarc.* TO mailuser@localhost IDENTIFIED BY '____mailUserPass';
 
+GRANT ALL PRIVILEGES ON roundcube.* TO mailuser@localhost IDENTIFIED BY '____mailUserPass';
+
 FLUSH PRIVILEGES;
 
 SET GLOBAL general_log = 'OFF';
