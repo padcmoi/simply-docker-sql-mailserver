99d762cde7d6df4446cd9cf2d7acafc15d44c9f2
padcmoi <juliennaskot@gmail.com>
2025-01-08 02:12:29 +0100

    feat: customizable jail rules

--- docker/exports/docker-entrypoint.sh
+++ docker/exports/docker-entrypoint.sh
@@ -14,8 +14,26 @@ if [ ! -f /.package-installed ]; then
     service fail2ban stop
     cp -f /opt/conf/fail2ban/* $FAIL2BAN_CONFIG_DIR/
     echo "" >$FAIL2BAN_CONFIG_DIR/jail.d/defaults-debian.conf
-    sed -i "s/loglevel = INFO/loglevel = NOTICE/" $FAIL2BAN_CONFIG_DIR/fail2ban.conf 
-    sed -i "s/logtarget = \/var\/log\/fail2ban.log/logtarget = SYSLOG/" $FAIL2BAN_CONFIG_DIR/fail2ban.conf 
+    sed -i "s/loglevel = INFO/loglevel = NOTICE/" $FAIL2BAN_CONFIG_DIR/fail2ban.conf
+    sed -i "s/logtarget = \/var\/log\/fail2ban.log/logtarget = SYSLOG/" $FAIL2BAN_CONFIG_DIR/fail2ban.conf
+
+    if [ $FAIL2BAN_MAXRETRY -gt 0 ]; then
+        sed -i "s/____fail2BanMaxRetry/${FAIL2BAN_MAXRETRY}/g" $FAIL2BAN_CONFIG_DIR/jail.local
+    else
+        sed -i "s/____fail2BanMaxRetry/30/g" $FAIL2BAN_CONFIG_DIR/jail.local
+    fi
+
+    if [ $FAIL2BAN_FINDTIME -gt 0 ]; then
+        sed -i "s/____fail2BanFindtime/${FAIL2BAN_FINDTIME}/g" $FAIL2BAN_CONFIG_DIR/jail.local
+    else
+        sed -i "s/____fail2BanFindtime/90/g" $FAIL2BAN_CONFIG_DIR/jail.local
+    fi
+
+    if [ $FAIL2BAN_BANTIME -gt 0 ]; then
+        sed -i "s/____fail2BanBantime/${FAIL2BAN_BANTIME}/g" $FAIL2BAN_CONFIG_DIR/jail.local
+    else
+        sed -i "s/____fail2BanBantime/3600/g" $FAIL2BAN_CONFIG_DIR/jail.local
+    fi
 fi
 
 # Mysql and create if not exists database
