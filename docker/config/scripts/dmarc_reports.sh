24dfd76568811756db83bd1575670e63c1787705
padcmoi <juliennaskot@gmail.com>
2025-01-13 08:50:15 +0100

    feat: add opendmarc with configuration

---
+++ docker/config/scripts/dmarc_reports.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# dmarc_reports.sh  v1.1  - March 6, 2019
+# Copyright Noel Butler <noelb@ausics.net> 2015-2019. All Rights Reserved.
+#
+# Licenced under GPL v3.0 - https://www.gnu.org/licenses/gpl-3.0.en.html
+# This entitles you to freely share or modify this scipt providing original
+# author and copyright information is retained.
+############################################################################
+
+# Import report data if any exists, normal to be missing in quiet periods of the day.
+
+if [ -f "/var/run/opendmarc.dat" ]; then
+	/usr/local/sbin/opendmarc-import --dbhost=localhost --dbname=opendmarc --dbpasswd=CHANGEME --dbuser=opendmarc --input=/var/run/opendmarc.dat
+	sleep 1
+	/usr/bin/rm /var/run/opendmarc.dat
+fi
+
+# Send daily reports, once, in reality the below works at to around 00:05
+
+currenttime=$(date +%H:%M)
+currentdom=$(date +%d)
+
+if [[ "$currenttime" > "00:00" ]] && [[ "$currenttime" < "00:08" ]]; then
+	/usr/bin/logger "Sending Reports"
+	/usr/local/sbin/opendmarc-reports --day --dbhost=localhost --dbname=opendmarc --dbpasswd=CHANGEME --dbuser=opendmarc --report-email=dmarc_reports@CHANGEME-YOURDOMAIN --smtp-port=25 --smtp-server=localhost
+
+	# Expire old reports once a month, keeping only 90 days to avoid bloated database.
+
+	if [ "$currentdom" == "01" ]; then
+		/usr/bin/logger "Expire"
+		/usr/local/sbin/opendmarc-expire --alltables --expire=90 --dbhost=localhost --dbname=opendmarc --dbpasswd=CHANGEME --dbuser=opendmarc
+	fi
+
+fi
--- docker/config/setup.d/28-dmarc.sh
+++ docker/config/setup.d/28-dmarc.sh
@@ -19,14 +19,12 @@ build)
             echo "configure dmarc with rspamd ..." && sleep 5
 
             sed -i "s/____dmarcEnable/${DMARC_ENABLE}/g" /etc/rspamd/local.d/dmarc.conf
-            sed -i "s/____dmarcReports/${DMARC_REPORTS}/g" /etc/rspamd/local.d/dmarc.conf
-            sed -i "s/____dmarcDomain/${DMARC_DOMAIN}/g" /etc/rspamd/local.d/dmarc.conf
-            sed -i "s/____dmarcOrgName/${DMARC_ORG_NAME}/g" /etc/rspamd/local.d/dmarc.conf
 
             ;;
 
         OpenDMARC)
-            echo "configure dmarc with OpenDMARC ..." && sleep 5
+
+            sed -i "s/____dmarcEnable/false/g" /etc/rspamd/local.d/dmarc.conf
 
             ;;
 
@@ -35,11 +33,12 @@ build)
     else
 
         sed -i "s/____dmarcEnable/false/g" /etc/rspamd/local.d/dmarc.conf
-        sed -i "s/____dmarcReports/${DMARC_REPORTS}/g" /etc/rspamd/local.d/dmarc.conf
-        sed -i "s/____dmarcDomain/${DMARC_DOMAIN}/g" /etc/rspamd/local.d/dmarc.conf
-        sed -i "s/____dmarcOrgName/${DMARC_ORG_NAME}/g" /etc/rspamd/local.d/dmarc.conf
 
     fi
+
+    sed -i "s/____dmarcReports/${DMARC_REPORTS}/g" /etc/rspamd/local.d/dmarc.conf
+    sed -i "s/____dmarcDomain/${DMARC_DOMAIN}/g" /etc/rspamd/local.d/dmarc.conf
+    sed -i "s/____dmarcOrgName/${DMARC_ORG_NAME}/g" /etc/rspamd/local.d/dmarc.conf
 
     ;;
 
@@ -57,7 +56,43 @@ retrieve-volume)
 
 container)
 
-    #
+    if [ ! $DMARC_ENABLE == false ] && [ $DMARC_REPORTS ] && ([ $DMARC_SELECT == "RSPAMD" ] || [ $DMARC_SELECT == "OpenDMARC" ]); then
+
+        case $DMARC_SELECT in
+
+        RSPAMD)
+
+            # action
+
+            ;;
+
+        OpenDMARC)
+
+            apt -y install dbconfig-common dbconfig-mysql dbconfig-sqlite3
+
+            # install opendmarc
+            /usr/share/debconf/fix_db.pl &&
+                echo "opendmarc opendmarc/dbconfig-install boolean true" | debconf-set-selections &&
+                echo "opendmarc opendmarc/dbconfig-reinstall boolean true" | debconf-set-selections &&
+                echo "opendmarc opendmarc/mysql/app-pass password ${MYSQL_ROOT_PASSWORD}" | debconf-set-selections &&
+                echo "opendmarc opendmarc/mysql/app-pass-confirm password ${MYSQL_ROOT_PASSWORD}" | debconf-set-selections &&
+                apt --assume-yes install opendmarc
+
+            # import and configure conf files
+
+            cp -R -f /docker-config/conf.d/opendmarc/* /etc/
+
+            sed -i "s/____domainFQDN/${DOMAIN_FQDN}/g" /etc/opendmarc.conf
+            sed -i "s/____dmarcReports/${DMARC_REPORTS}/g" /etc/opendmarc.conf
+
+            # run daemon
+            opendmarc -c /etc/opendmarc.conf
+
+            ;;
+
+        esac
+
+    fi
 
     ;;
 *)
